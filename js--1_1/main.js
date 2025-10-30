15dc65820c0a784bafde241ef2d5a32fa7439ca3
Marat217 <garifullinmarat217@gmail.com>
2025-10-30 17:27:30 +0300

    Add files via upload

---
+++ js--1_1/main.js
@@ -0,0 +1,200 @@
+
+// const firstName = 'John'
+// const lastName = 'kutak' 
+// let i = 20
+// console.log(firstName, lastName, i)
+// ======================================================================================================================
+// const instrument = 'Инструмент'
+// const box = 'ящике'
+// console.log(`${instrument} лежит в ${box}`) 
+// ======================================================================================================================
+// const user = 'User'
+// const age = 15
+// const isKutak = true
+// console.log(`${user} admin: ${isKutak}`)
+// console.log(typeof(user))
+// let foo
+// console.log(foo)
+// ======================================================================================================================
+// const firstNumber = 112
+// const secondNumber = 112
+// let result = null
+// if (firstNumber === 112 &&  secondNumber === 112 ) {
+//    result = firstNumber + secondNumber  
+//    console.log(result)
+// } else {
+//    result = secondNumber - firstNumber
+//    console.log(result)
+// }
+// ======================================================================================================================
+// const alex = 'admin'
+// if (alex === 'user') {
+//    console.log('alex is user')
+// } else if (alex === 'vip') {
+//    console.log('alex is vip')
+// } else {
+//    console.log('alex is admin')
+// }
+// ======================================================================================================================
+// const alex = 'admin'
+// alex === 'admin'? console.log('alex is admin') : console.log('alex is not admin')
+// ======================================================================================================================
+// let i = 0
+// do {
+//    i = i + 1
+//    console.log(i)
+// } while(i < 4)
+// for (let i = 1; i <= 8; i = i + 2) {
+//    console.log(`пройден ${i} круг`)
+// }
+// ======================================================================================================================
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// for (let i = 0; i < numbers.length; i++)
+// console.log(numbers[i])
+// ======================================================================================================================
+
+// function sumNumbers(firstNumber, secondNumber) {
+//    return firstNumber + secondNumber
+// }
+// const result = sumNumbers(3, 10)
+// console.log(result)
+// console.log(sumNumbers(-2, 2))
+
+// ======================================================================================================================
+
+// const users = ['Марат', 'Гузель', 'Аиша']
+// const numbers = [1, 2, 3]
+// const chekForCopyItem = (array, item) => {
+//    for (let i = 0; i < array.length; i++) {
+//       if (array[i] === item) {
+//          return `Копия ${item} в массиве есть`
+//       }
+//    }
+//    return 'Такого элемента в массиве нет.'
+// }
+// console.log(chekForCopyItem(users, 'Гузель'))
+
+// ======================================================================================================================
+
+// function namePlusHello(name) {
+//    return `Hello ${name}!`;
+// }
+// const result = namePlusHello('Марат')
+// console.log(result)
+
+// ======================================================================================================================
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+// function numberCrunching(numbers) {
+//    for (let i = 0; i < array.length; i++) {
+//       if (numbers[i] == 7) {
+//          console.log(numbers[i]);
+//       }
+//    }
+// }
+
+// ===============================================================================
+
+// const numbers = [6, 13, 14, 25, 2, 3]
+// function calculation(numbers) {
+//    for (let i = 0; i < numbers.length; i++) {
+//       if (numbers[i] > 10) {
+//       console.log(numbers[i]);
+//       }
+//    }
+// }
+// console.log(calculation(numbers));
+
+// ===============================================================================
+
+// function calc(number1, number2, sign) {
+//    let result = operationResult;
+//    if (sign === 'minus') {
+//       result = number1 - number2;
+//    }
+//    return result;
+// }
+// console.log(operationResult)
+
+// ===============================================================================
+
+// function calc(number1, number2, sign) {
+//    let result;
+//    if (sign === 'plus') {
+//       result = number1 + number2;
+//    }
+//    return result;
+// }
+// let operationResult = calc(2, 3, 'plus')
+
+// ===============================================================================
+
+// function calc(number1, number2, sign) {
+//    let result;
+//    if (sign === 'multiply') {
+//       result = number1 * number2;
+//    }
+//    return result;
+// }
+// let operationResult = calc(2, 3, 'multiply')
+// console.log(operationResult)
+
+// ===============================================================================
+
+// const calc = (number1, number2, sign) => {
+//    let result;
+//    if (sign === 'divide') {
+//       result = number1 / number2;
+//    }
+//    return result;
+// }
+// let operationResult = calc(2, 3, 'divide')
+// console.log(operationResult)
+
+// ===============================================================================
+// =============================ДОМАШНЯЯ РАБОТА===================================
+// ===============================================================================
+
+// const user = {
+//    name: 'Marat',
+//    age: 31,
+//    intelect: 'tupica'
+// }
+// console.log('Hello ' + user.name)
+
+// ===============================================================================
+
+// const users = [
+//    {
+//       name: 'Marat',  
+//       age: 31,
+//       intelect: 'tupica'
+//    },
+//    {
+//       name: 'Wife',  
+//       age: 30,
+//       intelect: 'smart'
+//    },
+
+//    {
+//       name: 'Child',    
+//       age: 2,
+//       intelect: 'smart'
+//    }
+// ]
+
+// users.push({
+//       name: 'grandmother',    
+//       age: 60,
+//       intelect: 'smart'
+// })
+
+// for (let i = 0; i < users.length; i++) {
+//    console.log(users[i])
+// }
+
+// ===============================================================================
+
+
+
+
